4bcfd92cc2a38462db510a6eedeb5a14560e5f38
Joowan Kim <rlawndhks217@gmail.com>
2022-01-16 12:42:01 +0900

    Pay class 수정
    Pay 인스턴스로 곱하기 연산이 가능하도록 수정하였습니다.

--- examples/ch07/pay.py
+++ examples/ch07/pay.py
@@ -6,3 +6,8 @@ class Pay:
     def __init__(self, amount: int):
         self.amount = amount
         self.unit = "KRW"
+
+    def __rmul__(self, other):
+        if isinstance(other, float) or isinstance(other, int):
+            return Pay(int(other * self.amount))
+        return Pay(other.amount * self.amount)
