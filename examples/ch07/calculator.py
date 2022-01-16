2d3ece81424f71aee260de88de71c917de71a7b4
Joowan Kim <rlawndhks217@gmail.com>
2022-01-16 12:20:35 +0900

    리팩터링

--- examples/ch07/calculator.py
+++ examples/ch07/calculator.py
@@ -7,4 +7,4 @@ class PayCalculator:
         self.tax_rate = tax_rate
 
     def pay_of(self, employee: Employee) -> Pay:
-        return employee.base_pay - employee.base_pay * 0.1
+        return (1 - self.tax_rate) * employee.base_pay
